ae6cb879d022dc5b22e42d2dddba35c1c3dfd146
Eugene Pyatigorsky <955982@luxgroup.net>
2017-10-25 13:44:05 -0400

    url hash

--- app.R
+++ app.R
@@ -220,9 +220,10 @@ server <- function(input, output, session) {
   })
   
   # observe({print(names(session$clientData))})
-  observe({cat("url hash:", session$clientData$url_hash, "\n",
-               "url hash initial:", session$clientData$url_hash_initial, "\n")})
-  
+  observe({cat("url hash:", session$clientData$url_hash)})
+  # , "\n",
+  #              "url hash initial:", session$clientData$url_hash_initial, "\n")})
+  # 
   observe({cat("url_search:", session$clientData$url_search, "\n")})
   
   
