756a491e26c91bca5683bb984066564b9077809d
Eugene Pyatigorsky <955982@luxgroup.net>
2017-10-26 01:49:55 -0400

    routing file for safekeeping (not needed anymore)
    routing code moved to R + 1 function in app.R

--- www/js/app.js
+++ www/js/app.js
@@ -1,27 +1,11 @@
-// $('document').ready(function() {
-//   // create some functions to be executed when
-//   // the correct route is issued by the user.
-//   var ShinyRoute = function(rt) {
-//     return function () {setTimeout(function () {
-//       Shiny.onInputChange('route_clicked', rt);
-//     }, 0);
-//   }};
-//
-//   // define the routing table.
-//   var routes = {
-//     %s
-//   };
-//
-//   // instantiate the router.
-//   var router = Router(routes);
-//   router.init('/');
-//
-//   $('#searchText').keypress(function (e) {
-//     if (e.which == 13) {
-//       setTimeout(function () { Shiny.onInputChange('searchSubmit', Math.random()); }, 0);
-//       router.setRoute('/search')
-//     }
-//   });
-//
-//   $('.close-search, .clear-search').click();
-// });
+$('document').ready(function() {
+
+  // Route search form enter submission to window url
+  var searchText = $('#searchText')
+  searchText.keypress(function (e) {
+    if (e.which == 13) {
+      window.location.href = '#/search?term=' + searchText.val();
+      $('.close-search, .clear-search').click();
+    }
+  });
+});
---
+++ www/js/routing.js
@@ -0,0 +1,32 @@
+$('document').ready(function() {
+  // create some functions to be executed when
+  // the correct route is issued by the user.
+  var ShinyRoute = function(rt) {
+    return function () {setTimeout(function () {
+      Shiny.onInputChange('route_clicked', rt);
+      }, 0);
+    }
+  };
+
+  // define the routing table.
+  var routes = {
+    %s
+  };
+
+  // instantiate the router.
+  var router = Router(routes);
+  router.init('/');
+
+  $('#searchText').keypress(function (e) {
+      if (e.which == 13) {
+        setTimeout(function () {
+          Shiny.onInputChange('searchSubmit', Math.random());
+          $('.close-search, .clear-search').click();
+        }, 0);
+        var term = $('#searchText').val();
+        alert(term);
+        router.setRoute('/search?term=' + term);
+      }
+  });
+  router.init('/');
+});
